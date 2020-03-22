be7a34fefab3397930bd860bfc127bd5a0d312ad
kkfullsd <kir2915@gmail.com>
2020-03-22 19:55:48 +0300

    Фикс счетчика в посиске открытых групп, редизайн поиска по открытым группам

--- src/GroupsCanPost/GroupsCanPost.js
+++ src/GroupsCanPost/GroupsCanPost.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
+import classes from './GroupsCanPost.module.scss'
 
 const GropsCanPost = () => {
 
-   let [state, changeState] = useState([])
-
-   let [filtered, changeFiltered] = useState([])
+   const [state, changeState] = useState([])
+   const [filtered, changeFiltered] = useState([])
 
    let filterGroups = () => {
     
@@ -12,13 +12,14 @@ const GropsCanPost = () => {
             let istart = i*500;
             let ifinish = istart+500;
             let groups = state.slice(istart, ifinish).join(',')
-            console.log(groups)
 
             window.VK.Api.call('groups.getById', {group_ids: groups, fields: 'can_post', v:'5.73' }, (r)=>{
                 if (r.response) {
-                    console.log(r.response)
-                    //changeFiltered(filtered.concat(r.response))
-                    changeFiltered((prevState)=>prevState.concat(r.response))
+                    r.response.map((group, index)=>{
+                        if (group.can_post === 1) {
+                            changeFiltered((prevState)=>prevState.concat(group))
+                        }
+                    })
                 }
             })
         }
@@ -27,28 +28,29 @@ const GropsCanPost = () => {
 
 
     return(
-        <div>
-            <h2>Вставьте группы, которые нужно отсортировать по открытой стене</h2>
+        <div className={classes.container}>
+            <label for='initialGroups'>Вставьте группы, которые нужно отсортировать по открытой стене</label>
             <textarea 
+                id='initialGroups'
                 name='initialGroups' 
                 onChange={event=>{
-                    //changeState(event.target.value)
                     changeState(event.target.value.split('\nhttps://vk.com/'))
-                    console.log(state)
                 }}
                 /> 
-            <button onClick={filterGroups} >Отсоритировать</button> 
-            <h3>Всего групп с открытой стеной получено: {filtered.length}</h3>
-            <div style={{width: '450px', height:'500px', border:'1px solid gray', textAlign:'left'}}> 
+            <button className={classes.sortButton} onClick={filterGroups} >Отсортировать</button> 
+            <h3 className={classes.counter} >Всего групп с открытой стеной получено: {filtered.length}</h3>
+            <div className={classes.result}> 
+            
                 {filtered.map((group, index)=>{
-                if (group.can_post === 1) {
                     return (
                         <div key={index} >
                         {'https://vk.com/'+group.screen_name}
                         </div>
                     )
-                }
-            })}</div>
+                
+            })}
+            
+            </div>
 
 
         </div>
