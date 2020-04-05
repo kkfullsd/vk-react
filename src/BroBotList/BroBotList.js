1a26aff679140e40edea44fa9c500735f60d5b10
kkfullsd <kir2915@gmail.com>
2020-04-05 18:58:10 +0300

    Фикс списка для бробота

--- src/BroBotList/BroBotList.js
+++ src/BroBotList/BroBotList.js
@@ -7,22 +7,13 @@ let BroBotList = () => {
    const [phrase, changePhrase] = useState([])
    const [output, changeOutput] = useState([])
 
-
-//    function randomInteger(min, max) {
-//     // получить случайное число от (min-0.5) до (max+0.5)
-//     let rand = min - 0.5 + Math.random() * (max - min + 1);
-//     return Math.round(rand);
-//   }
-
   function randomInteger(min, max) {
-    // случайное число от min до (max+1)
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
    let combine = () => {
        let final = []
-        for (let i=0; i<groups.length; i++) {
+        for (let i=groups.length-1; i>=0; i--) {
             console.log(randomInteger(0, phrase.length-1))
             let result = groups[i] + ' ' + phrase[randomInteger(0, phrase.length-1)] + ' [image:image.jpg]'
             final.push(result)
@@ -37,16 +28,15 @@ let BroBotList = () => {
             <div className={classes.control}>
             <label className={classes.label} htmlFor='groups'>Вставьте список групп</label>
             <textarea className={classes.textarea}  onChange={(event)=>{
-                let arr = event.target.value.split('\n')
-                changeGroups(prevState=>prevState.concat(arr))
+                changeGroups(event.target.value.split('\n'))
+                
             }} id='groups' />
             </div>
 
             <div className={classes.control}>
             <label className={classes.label}  htmlFor='phrase'>Вставьте список фраз</label>
             <textarea className={classes.textarea}  onChange={(event)=>{
-                let arr = event.target.value.split('\n')
-                changePhrase(prevState=>prevState.concat(arr))
+                changePhrase(event.target.value.split('\n'))
             }} id='phrase' />
             </div>
 
@@ -58,9 +48,9 @@ let BroBotList = () => {
 
 
             <div className={classes.output}>
-                {output.length > 0 ? output.map(str=>{
+                {output.length > 0 ? output.map((str, index)=>{
                     return(
-                        <div>
+                        <div key={index}>
                             {str}
                         </div>
                     )
