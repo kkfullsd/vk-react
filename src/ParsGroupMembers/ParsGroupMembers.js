4842bd40c719def0c398c7d16658024cae1bf190
kkfullsd <kir2915@gmail.com>
2020-04-07 23:05:44 +0300

    Стабильная версия парсера участников групп

--- src/ParsGroupMembers/ParsGroupMembers.js
+++ src/ParsGroupMembers/ParsGroupMembers.js
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import classes from './ParsGroupMembers.module.scss'
+import loader from '../ui/loader/loader.module.scss'
 
 let ParsGroupMembers = props => {
 
     const [state, changeState] = useState([])
+    const [state2, changeState2] = useState([])
     const [output, changeOutput] = useState([])
     const [isValid, changeIsValid] = useState(true)
+    const [isLoading, changeIsLoading] = useState(false)
+
    // const [counter, changeCounter] = useState(0)
     const [aimer, changeAimer] = useState(0)
 
 
 
-    let search = (q) => {
+    let search = (q, func) => {
+
         let counter = 0
+   
 
         window.VK.Api.call('groups.getMembers', {group_id: q, v:'5.73' }, (r)=>{ if (r.response) {
-            let num = Math.ceil(r.response.count / 1000)
-            counter = num
+            counter = Math.ceil(r.response.count / 1000)
             iterate(0)
             }             
         })
@@ -24,27 +29,50 @@ let ParsGroupMembers = props => {
         let iterate = (num) => {
             if (num < counter) {
                window.VK.Api.call('groups.getMembers', {group_id: q, offset:num*1000, v:'5.73' }, (r)=>{ if (r.response) {
-                console.log(q, ' ', r.response)
+                changeOutput(prevState=>prevState.concat(r.response.items))
                 setTimeout(()=>iterate(num+1), 500)
                 }             
                 }) 
             } else {
-                //????????????
-            }
-            
+                console.log('im here')
+                return func()
+            }   
         }
 
     }
+
+    function starter(arr) {
+        changeIsLoading(true)
+        let counter2 = 0
+
+        let innerStarter = () =>{
+            search(arr[counter2], ()=>{
+                if (counter2 < arr.length-1) {
+                    counter2 += 1
+                    innerStarter()
+                } else {
+                    changeIsLoading(false)
+                }
+            })
+        }
+
+        innerStarter()
+        
+        
+    }
+
 
 
 
 
     let filter = () => {
         if (state.length !== 0) {
+            let qs = []
             state.forEach((group)=>{
                 let q = group.startsWith('https://vk.com/club') ? group.slice(19) : group.slice(15)
-                search(q)
+                qs.push(q)
             })
+            starter(qs)
 
         } else {
             changeIsValid(false)
@@ -61,10 +89,29 @@ let ParsGroupMembers = props => {
                 }}
             />
 
-            <button className={classes.button} onClick={()=>filter()} >Начать сбор участников групп</button>
-            <div className={classes.output}>
+                {isLoading? null : 
+                    <button className={classes.button} onClick={()=>filter()} >Начать сбор участников групп</button>
+                }
+                
+                 {isLoading ? 
+                    <div className={loader.loader}></div> : null
+                    
+                }
+            
+                {output.length === 0 || isLoading ? 
+                    null :
+                    <div className={classes.output}>
+                        {output.map((id, index)=>{
+                            return(
+                                <div key={index}>
+                                    https://vk.com/id{id}
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
 
-            </div>
+            
         </div>
     )
 }
