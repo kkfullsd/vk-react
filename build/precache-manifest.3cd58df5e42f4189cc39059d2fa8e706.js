945c347565a6130e44f6db448dc0b7a847f7e5d5
kkfullsd <kir2915@gmail.com>
2020-07-23 20:23:42 +0300

    Build убран из gitignore

---
+++ build/precache-manifest.3cd58df5e42f4189cc39059d2fa8e706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff10f0a5a9d1534c54c48216fb513108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7898e0d56a55d606ebd",
+    "url": "/static/css/main.e6c06a6a.chunk.css"
+  },
+  {
+    "revision": "b878c7ee9ed3c6afe8df",
+    "url": "/static/js/2.28654f1d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.28654f1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7898e0d56a55d606ebd",
+    "url": "/static/js/main.727ee1d6.chunk.js"
+  },
+  {
+    "revision": "ca68da21649610549563",
+    "url": "/static/js/runtime-main.c9fe89e9.js"
+  }
+]);
