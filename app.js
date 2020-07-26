5e530f8a254ec4782098e9dc5d61d7de01c27a6b
kkfullsd <kir2915@gmail.com>
2020-07-26 18:48:25 +0300

    Добавлен ssl сертификат

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const https = require('https');
+const fs = require('fs');
 
 
 const app = express()
@@ -8,17 +10,23 @@ app.use(express.json({ extended: true }))
 
 app.use('/', express.static(path.join(__dirname, 'build')))
 
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 let PORT = 80
 
 async function start() {
     try {
-       app.listen(PORT, () =>
-         console.log(`app has been started on port: ${PORT}...`)
-       );
+      https.createServer({
+        key: fs.readFileSync('./ssl/key.pem'),
+        cert: fs.readFileSync('./ssl/cert.crt'),
+      },
+        app).listen(PORT)
+      console.log('Server is listening :', PORT)
+      //  app.listen(PORT, () =>
+      //    console.log(`app has been started on port: ${PORT}...`)
+      //  );
     } catch (e) {
         console.error(e.message)
         process.exit(1)
